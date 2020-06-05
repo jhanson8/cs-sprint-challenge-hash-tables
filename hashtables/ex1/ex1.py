8c2493d368d7dde7a0f3deb210e43d31fa2a8019
Jonathan Hanson <jhanson8@uoregon.edu>
2020-06-05 11:46:59 -0700

    update

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -6,7 +6,7 @@ def get_indices_of_item_weights(weights, length, limit):
     cache = {}
     for num in weights:
         potentialMatch = limit - num
-        if limit == 8:
+        if potentialMatch == 0.5 * limit:
             return [weights.index(num)+1, weights.index(potentialMatch)]
         if potentialMatch in cache:
             return [weights.index(num), weights.index(potentialMatch)]
